0def664cc46b3704d06d2142defb470bcef50c6a
Circuit-Overtime <74301576+Circuit-Overtime@users.noreply.github.com>
2021-01-06 17:07:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 900);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	paperObject=new paper(292,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	//Create a Ground
+	
+
+
+
+	Engine.run(engine);
+	//Render.run(render);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObj.display();
+
+  
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:139,y:-145});
+	}
+}
+
+
+
+
+
